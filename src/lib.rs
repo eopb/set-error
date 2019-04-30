057a99fe0db79a5071394c05a7b4ce517e488975
Ethan Brierley <ethanboxx@gmail.com>
2019-04-30 14:48:16 +0100

    add code

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![deny(clippy::pedantic)]
+#![warn(missing_docs)]
+
+pub trait ChangeError<T> {
+    fn set_error(self, s: &str) -> Result<T, String>;
+}
+
+impl<T, E> ChangeError<T> for Result<T, E> {
+    fn set_error(self, s: &str) -> Result<T, String> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(_) => Err(s.to_string()),
+        }
+    }
+}
+impl<T> ChangeError<T> for Option<T> {
+    fn set_error(self, s: &str) -> Result<T, String> {
+        match self {
+            Some(t) => Ok(t),
+            None => Err(s.to_string()),
+        }
+    }
+}
